df04e5476d2fd785a230ddbda7aca8215bb643e5
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2016-12-31 17:08:02 -0300

    add model locales

--- app/models/major.rb
+++ app/models/major.rb
@@ -4,7 +4,7 @@
 #
 #  id                :integer          not null, primary key
 #  name              :string
-#  type              :string
+#  category          :string
 #  description       :text
 #  short_description :text
 #  video_url_code    :string
